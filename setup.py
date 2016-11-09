43183202eaa0db2049a6b1461bdcb9256272f7be
Fabian Latorre <latorrefabian@gmail.com>
2016-11-09 16:11:07 -0500

    ver 0.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='topmine',
-      version='0.2',
+      version='0.2.1',
       description='topmine python implementation',
       author='Fabian Latorre',
       author_email='latorrefabian@gmail.com',
